5bfb7df43649a188eb40acc07e13d7df7a4f9009
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-07-08 17:48:16 +0530

    add theme toggle with next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist_Mono, Inter } from 'next/font/google'
 
 import { cn } from '@/lib/utils'
+import { Providers } from '@/providers'
 
 import './globals.css'
 
@@ -30,8 +31,11 @@ export default function RootLayout({
         geistMono.variable,
         inter.variable,
       )}
+      suppressHydrationWarning
     >
-      <body className='min-h-full flex flex-col'>{children}</body>
+      <body className='min-h-full flex flex-col'>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from '@/components/ui/button'
+import { ThemeToggle } from '@/components/theme/theme-toggle'
 
 export default function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
-      <Button>Click me!</Button>
+      <ThemeToggle />
     </div>
   )
 }
