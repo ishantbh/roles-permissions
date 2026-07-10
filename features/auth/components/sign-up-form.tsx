5c58db1fdcde59c0db02659c54ba1cd5481d2ed2
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-07-10 21:30:54 +0530

    update sign-up form to add validation and submission

--- features/auth/components/sign-up-form.tsx
+++ features/auth/components/sign-up-form.tsx
@@ -1,6 +1,14 @@
+'use client'
+
 import Link from 'next/link'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Controller, useForm } from 'react-hook-form'
 
 import { cn } from '@/lib/utils'
+import {
+  SignUpFormData,
+  signUpSchema,
+} from '../validation/sign-up-form-schema copy'
 import { Button } from '@/components/ui/button'
 import {
   Card,
@@ -12,15 +20,34 @@ import {
 import {
   Field,
   FieldDescription,
+  FieldError,
   FieldGroup,
   FieldLabel,
 } from '@/components/ui/field'
 import { Input } from '@/components/ui/input'
+import { Spinner } from '@/components/ui/spinner'
 
 export function SignUpForm({
   className,
   ...props
 }: React.ComponentProps<'div'>) {
+  const form = useForm<SignUpFormData>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  })
+
+  const { isSubmitting } = form.formState
+
+  async function onSubmit(data: SignUpFormData) {
+    // TODO: Handle login
+    console.log('Submitted data:', data)
+  }
+
   return (
     <div className={cn('flex flex-col gap-6', className)} {...props}>
       <Card>
@@ -31,34 +58,100 @@ export function SignUpForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
             <FieldGroup>
+              <Controller
+                name='name'
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor='name'>Name</FieldLabel>
+                    <Input
+                      {...field}
+                      id='name'
+                      type='text'
+                      placeholder='John Doe'
+                      aria-invalid={fieldState.invalid}
+                      disabled={isSubmitting}
+                      required
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name='email'
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor='email'>Email</FieldLabel>
+                    <Input
+                      {...field}
+                      id='email'
+                      type='email'
+                      placeholder='m@example.com'
+                      aria-invalid={fieldState.invalid}
+                      disabled={isSubmitting}
+                      required
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name='password'
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor='password'>Password</FieldLabel>
+                    <Input
+                      {...field}
+                      id='password'
+                      type='password'
+                      aria-invalid={fieldState.invalid}
+                      disabled={isSubmitting}
+                      required
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name='confirmPassword'
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor='confirm-password'>
+                      Confirm Password
+                    </FieldLabel>
+                    <Input
+                      {...field}
+                      id='confirm-password'
+                      type='password'
+                      aria-invalid={fieldState.invalid}
+                      disabled={isSubmitting}
+                      required
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+
               <Field>
-                <FieldLabel htmlFor='name'>Name</FieldLabel>
-                <Input id='name' type='text' placeholder='John Doe' required />
-              </Field>
-              <Field>
-                <FieldLabel htmlFor='email'>Email</FieldLabel>
-                <Input
-                  id='email'
-                  type='email'
-                  placeholder='m@example.com'
-                  required
-                />
-              </Field>
-              <Field>
-                <FieldLabel htmlFor='password'>Password</FieldLabel>
-                <Input id='password' type='password' required />
-              </Field>
-              <Field>
-                <FieldLabel htmlFor='confirm-password'>
-                  Confirm Password
-                </FieldLabel>
-                <Input id='confirm-password' type='password' required />
-              </Field>
-              <Field>
-                <Button type='submit'>Sign Up</Button>
-                <Button variant='outline' type='button'>
+                <Button type='submit' disabled={isSubmitting}>
+                  {isSubmitting && <Spinner data-icon='inline-start' />}
+                  <span>Sign Up</span>
+                </Button>
+
+                <Button variant='outline' type='button' disabled={isSubmitting}>
                   Login with Google
                 </Button>
                 <FieldDescription className='text-center'>
