345cf63534338fbc19b2d9bee2a27ad45dbd7583
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-07-11 14:09:15 +0530

    add basic landing-page content

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,6 +4,16 @@ export default function HomePage() {
   return (
     <div className='min-h-screen w-full flex flex-col'>
       <Header />
+
+      <main className='w-full max-w-3xl mx-auto p-4 grow flex'>
+        <div className='flex flex-col items-center justify-center gap-4 w-full text-center'>
+          <h1 className='text-5xl font-semibold'>Roles & Permissions</h1>
+          <p className='text-xl text-muted-foreground'>
+            Better Auth organizations with roles and permissions in Next.js with
+            user invitations, role based access control, and more.
+          </p>
+        </div>
+      </main>
     </div>
   )
 }
