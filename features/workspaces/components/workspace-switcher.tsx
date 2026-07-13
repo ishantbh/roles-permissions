4668566267a81c71c447bf378767b136f98f1f06
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-07-13 20:19:06 +0530

    update workspace-switcher to trigger dialog to create new workspaces

--- features/workspaces/components/workspace-switcher.tsx
+++ features/workspaces/components/workspace-switcher.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
 import { CheckIcon, ChevronsUpDownIcon, PlusIcon } from 'lucide-react'
@@ -8,6 +9,14 @@ import { cn } from '@/lib/utils'
 import { authClient } from '@/lib/auth/auth-client'
 import { Button } from '@/components/ui/button'
 import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
@@ -15,9 +24,12 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import { WorkspaceForm } from './workspace-form'
 
 export function WorkspaceSwitcher() {
   const router = useRouter()
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
 
   const { data: workspaces } = authClient.useListOrganizations()
   const { data: activeWorkspace } = authClient.useActiveOrganization()
@@ -49,43 +61,62 @@ export function WorkspaceSwitcher() {
   }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant='outline' className='px-4 w-full max-w-3xs'>
-          <div className='text-sm leading-tight'>
-            <span className='truncate font-medium'>
-              {activeWorkspace?.name}
-            </span>
-          </div>
-          <ChevronsUpDownIcon className='ml-auto' />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel className='text-xs text-muted-foreground'>
-          Workspaces
-        </DropdownMenuLabel>
-        {workspaces?.map((workspace) => {
-          const isActiveWorkspace = workspace.id === activeWorkspace?.id
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='outline' className='px-4 w-full max-w-3xs'>
+            <div className='text-sm leading-tight'>
+              <span className='truncate font-medium'>
+                {activeWorkspace?.name}
+              </span>
+            </div>
+            <ChevronsUpDownIcon className='ml-auto' />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel className='text-xs text-muted-foreground'>
+            Workspaces
+          </DropdownMenuLabel>
+          {workspaces?.map((workspace) => {
+            const isActiveWorkspace = workspace.id === activeWorkspace?.id
 
-          return (
-            <DropdownMenuItem
-              key={workspace.id}
-              onClick={() => setActiveWorkspace(workspace.id)}
-              className={cn('gap-2 p-2 justify-between')}
-            >
-              <span>{workspace.name}</span>
-              {isActiveWorkspace && <CheckIcon className='size-4' />}
-            </DropdownMenuItem>
-          )
-        })}
-        <DropdownMenuSeparator />
-        <DropdownMenuItem className='gap-2 p-2'>
-          <div className='flex size-6 items-center justify-center rounded-md border bg-transparent'>
-            <PlusIcon className='size-4' />
-          </div>
-          <div className='font-medium text-muted-foreground'>Add Workspace</div>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+            return (
+              <DropdownMenuItem
+                key={workspace.id}
+                onClick={() => setActiveWorkspace(workspace.id)}
+                className={cn('gap-2 p-2 justify-between')}
+              >
+                <span>{workspace.name}</span>
+                {isActiveWorkspace && <CheckIcon className='size-4' />}
+              </DropdownMenuItem>
+            )
+          })}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem className='gap-2 p-2' asChild>
+            <DialogTrigger asChild>
+              <Button
+                variant='ghost'
+                size='lg'
+                className='w-full justify-start hover:bg-accent aria-expanded:bg-accent dark:hover:bg-accent'
+              >
+                <PlusIcon className='size-4' />
+                <span>Add Workspace</span>
+              </Button>
+            </DialogTrigger>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <DialogContent className='sm:max-w-sm'>
+        <DialogHeader>
+          <DialogTitle>Create Workspace</DialogTitle>
+          <DialogDescription>
+            Create a new workspace to organize your projects.
+          </DialogDescription>
+        </DialogHeader>
+
+        <WorkspaceForm onClose={() => setIsDialogOpen(false)} />
+      </DialogContent>
+    </Dialog>
   )
 }
