cd7c432ba924066171690d805f5ba797c354d05e
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-07-17 13:01:53 +0530

    add dummy page for accept-invitation

---
+++ app/(app)/accept-invitation/[token]/page.tsx
@@ -0,0 +1,44 @@
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { auth } from '@/lib/auth'
+
+type AcceptInvitationPageProps = {
+  params: Promise<{ token: string }>
+}
+
+export default async function AcceptInvitationPage({
+  params,
+}: AcceptInvitationPageProps) {
+  const { token } = await params
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  if (!session) {
+    redirect(
+      `/login?redirect=/accept-invitation?token=${encodeURIComponent(token)}`,
+    )
+  }
+
+  const invitation = await auth.api.getInvitation({
+    query: {
+      id: token,
+    },
+    headers: await headers(),
+  })
+
+  return (
+    <div className='grow flex flex-col items-center gap-4 mt-4'>
+      <h1 className='text-2xl sm:text-3xl font-semibold text-center'>
+        Welcome {session.user.name}!
+      </h1>
+
+      <h2 className='text-lg sm:text-xl font-medium text-center'>
+        You have been invited to join:{' '}
+        <strong>{invitation.organizationName}</strong>
+      </h2>
+    </div>
+  )
+}
