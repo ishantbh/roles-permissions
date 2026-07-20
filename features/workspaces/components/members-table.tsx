1a93812827d326ded3dbd423e62684fd8c94e941
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-07-20 20:15:56 +0530

    display member-table actions in members-table component

--- features/workspaces/components/members-table.tsx
+++ features/workspaces/components/members-table.tsx
@@ -1,5 +1,4 @@
 import { headers } from 'next/headers'
-import { PencilIcon, Trash2Icon } from 'lucide-react'
 
 import { auth } from '@/lib/auth'
 import {
@@ -11,7 +10,7 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import { Button } from '@/components/ui/button'
+import { MembersTableActions } from './members-table-actions'
 
 export async function MembersTable() {
   const { members } = await auth.api.listMembers({
@@ -38,16 +37,7 @@ export async function MembersTable() {
             <TableCell>{member.user.email}</TableCell>
             <TableCell>{member.role}</TableCell>
             <TableCell className='text-right'>
-              <div className='flex items-center gap-3 justify-end'>
-                <Button variant='outline' size='icon' title='Edit'>
-                  <PencilIcon className='size-4' />
-                  <span className='sr-only'>Edit</span>
-                </Button>
-                <Button variant='destructive' size='icon' title='Delete'>
-                  <Trash2Icon className='size-4' />
-                  <span className='sr-only'>Delete</span>
-                </Button>
-              </div>
+              <MembersTableActions />
             </TableCell>
           </TableRow>
         ))}
