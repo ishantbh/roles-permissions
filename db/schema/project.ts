aac08652b3d2cd020635efb30ea583d71d9307a6
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-07-21 20:21:46 +0530

    update project schema to include indexes for table

--- db/schema/project.ts
+++ db/schema/project.ts
@@ -1,21 +1,35 @@
-import { pgTable, text, timestamp, uuid, varchar } from 'drizzle-orm/pg-core'
+import {
+  index,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core'
 
 import { organization } from './organization'
 import { user } from './user'
 
-export const project = pgTable('project', {
-  id: uuid().primaryKey().defaultRandom(),
-  orgId: text('org_id')
-    .notNull()
-    .references(() => organization.id, { onDelete: 'cascade' }),
-  userId: text('user_id')
-    .notNull()
-    .references(() => user.id, { onDelete: 'cascade' }),
-  title: varchar({ length: 255 }).notNull(),
-  description: text(),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at')
-    .defaultNow()
-    .$onUpdate(() => /* @__PURE__ */ new Date())
-    .notNull(),
-})
+export const project = pgTable(
+  'project',
+  {
+    id: uuid().primaryKey().defaultRandom(),
+    orgId: text('org_id')
+      .notNull()
+      .references(() => organization.id, { onDelete: 'cascade' }),
+    userId: text('user_id')
+      .notNull()
+      .references(() => user.id, { onDelete: 'cascade' }),
+    title: varchar({ length: 255 }).notNull(),
+    description: text(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at')
+      .defaultNow()
+      .$onUpdate(() => /* @__PURE__ */ new Date())
+      .notNull(),
+  },
+  (table) => [
+    index('project_orgId_idx').on(table.orgId),
+    index('project_userId_idx').on(table.userId),
+  ],
+)
