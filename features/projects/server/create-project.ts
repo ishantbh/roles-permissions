e87679447e52fb30e2450c015a4feeb9e4bd8ed2
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-07-22 20:39:24 +0530

    add server-action to create project

---
+++ features/projects/server/create-project.ts
@@ -0,0 +1,48 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { headers } from 'next/headers'
+
+import { db } from '@/db'
+import { project } from '@/db/schema'
+import { auth } from '@/lib/auth'
+import { projectSchema, type ProjectData } from '../validation/project-schema'
+
+export async function createProject(data: ProjectData) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  if (!session) {
+    return { error: 'Unauthorized' }
+  }
+
+  const member = await auth.api.getActiveMember({
+    headers: await headers(),
+  })
+
+  if (!member) {
+    return {
+      error: 'Current user not a member of the active organization',
+    }
+  }
+
+  // NOTE: No need to check permissions here, all users are allowed to create projects
+
+  const parsed = projectSchema.safeParse(data)
+
+  if (!parsed.success) {
+    return { error: 'Invalid inputs' }
+  }
+
+  const { title, description } = parsed.data
+
+  await db.insert(project).values({
+    title,
+    description,
+    orgId: member.organizationId,
+    userId: member.userId,
+  })
+
+  revalidatePath('/projects')
+}
