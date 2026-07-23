d22776d26493ff6bfc587c221802b9d838017735
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-07-23 14:31:57 +0530

    update project delete button to component to include logic to delete the projects

--- features/projects/components/project-list-item-delete-button.tsx
+++ features/projects/components/project-list-item-delete-button.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import { toast } from 'sonner'
 import { Trash2Icon } from 'lucide-react'
 
 import type { Project } from '@/db/types'
+import { deleteProject } from '../server/delete-project'
 import { Button } from '@/components/ui/button'
 
 type ProjectListItemDeleteButtonProps = {
@@ -12,11 +14,28 @@ type ProjectListItemDeleteButtonProps = {
 export function ProjectListItemDeleteButton({
   project,
 }: ProjectListItemDeleteButtonProps) {
+  async function handleDelete() {
+    try {
+      const res = await deleteProject({ projectId: project.id })
+
+      if (res?.error) {
+        toast.error(res.error)
+        return
+      }
+
+      toast.success('Project deleted')
+    } catch (err) {
+      toast.error('Error deleting project')
+    }
+  }
+
   return (
     <Button
       variant='destructive'
       size='icon'
       className='bg-inherit dark:bg-inherit'
+      onClick={handleDelete}
+      title='Delete'
     >
       <Trash2Icon className='size-4' />
       <span className='sr-only'>Delete {project.title}</span>
