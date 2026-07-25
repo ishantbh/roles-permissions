3e3e598b661871cf63d0f726f86968a910984f5c
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-07-25 14:01:16 +0530

    update project detail page to display project info

--- app/(app)/projects/[id]/page.tsx
+++ app/(app)/projects/[id]/page.tsx
@@ -48,5 +48,19 @@ export default async function ProjectDetailsPage({
     )
   }
 
-  return <div>{project.title}</div>
+  return (
+    <div className='grow flex flex-col items-center gap-4 mt-4'>
+      <div className='w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between gap-4'>
+        <div className='space-y-4'>
+          <h1 className='text-2xl sm:text-3xl font-semibold'>
+            {project.title}
+          </h1>
+
+          <p className='text-muted-foreground'>
+            {project.description ?? 'No description'}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
